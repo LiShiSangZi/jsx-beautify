72e4dc513538db079704fa5b1a1a4fc91adc08b9
gogocrow <gogocrow@163.com>
2017-01-24 13:13:10 +0800

    apply prettydiff as the formatter

--- extension.js
+++ extension.js
@@ -1,27 +1,77 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-var vscode = require('vscode');
+'use strict';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const vscode = require('vscode');
+const {
+  workspace
+} = vscode;
+const prettydiff = require('prettydiff');
+const fs = require('fs');
+
+const ACCEPTED_LANG = {
+  "javascript": true,
+  "javascriptreact": true
+};
+
+const getRange = (doc) => {
+  const r = new vscode.Range(0, 0, Number.MAX_VALUE, Number.MAX_VALUE);
+  return doc.validateRange(r);
+};
+
+function readConfig(key) {
+  let config = workspace.getConfiguration('gogocrow.beautify');
+  if (typeof config === 'undefined') {
+    config = {};
+  }
+  return config[key];
+}
+
+function formatActiveDocument() {
+  const active = vscode.window.activeTextEditor;
+  if (active && active.document) {
+    const doc = active.document;
+    const langId = doc.languageId;
+    if (ACCEPTED_LANG[langId]) {
+      // Do something.
+      const globalConfigPath = readConfig('jsbeautifyrc') || '';
+      let globalOptions = {};
+      try {
+        let content = fs.readFileSync(globalConfigPath, "utf8");
+        globalOptions = JSON.parse(content);
+      } catch (e) {
+        // Do nothing if the folder is invalid;
+      }
+
+      let text = doc.getText(new vscode.Range(0, 0, Number.MAX_VALUE, Number.MAX_VALUE));
+      let args = {
+        source: text,
+        insize: 2,
+        inchar: ' ',
+        lang: 'jsx',
+        mode: 'beautify',
+        bracepadding: false,
+        wrap: 80
+      };
+
+      args = Object.assign(args, globalOptions.jsx);
+
+      let output = prettydiff.api(args);
+      let outputText = output[0];
+      let range = getRange(active.document);
+      active.edit(editor => editor.replace(range, outputText));
+    }
+
+  }
+  return 0;
+}
+
+
 function activate(context) {
 
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "jsx-beautify" is now active!');
+  let sub = context.subscriptions;
+  sub.push(vscode.commands.registerCommand('gogocrow.beautify', formatActiveDocument));
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
-  var disposable = vscode.commands.registerCommand('extension.sayHello', function() {
-    // The code you place here will be executed every time your command is executed
-
-    // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World!');
-  });
-
-  context.subscriptions.push(disposable);
 }
+
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
